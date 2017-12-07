bc721744adc3bd1c0b166366a7e74445ea1a1d34
Pablo H. Paladino <paladinop@gmail.com>
2017-12-07 18:05:06 -0300

    open graph + share links

--- landing/app/scripts/main.js
+++ landing/app/scripts/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+	/*Counter*/
 	$("#counter")
 		.countdown("2018/01/08 19:30:00", function(event) {
 			$(this).html(
@@ -12,4 +13,36 @@ $(document).ready(function() {
 		})
 		.on("update.countdown", function(event) {})
 		.on("finish.countdown", function(event) {});
+
+	/*SHARE*/
+	var popupSize = {
+		width: 780,
+		height: 550
+	};
+
+	$(document).on("click", ".social-button", function(e) {
+		var verticalPos = Math.floor(($(window).width() - popupSize.width) / 2),
+			horisontalPos = Math.floor(
+				($(window).height() - popupSize.height) / 2
+			);
+
+		var popup = window.open(
+			$(this).prop("href"),
+			"social",
+			"width=" +
+				popupSize.width +
+				",height=" +
+				popupSize.height +
+				",left=" +
+				verticalPos +
+				",top=" +
+				horisontalPos +
+				",location=0,menubar=0,toolbar=0,status=0,scrollbars=1,resizable=1"
+		);
+
+		if (popup) {
+			popup.focus();
+			e.preventDefault();
+		}
+	});
 });
