ccfb74e69fdf9f5e9152a47c9cf333abb84e588d
Pablo H. Paladino <paladinop@gmail.com>
2017-12-07 15:38:25 -0300

    initial commit

---
+++ landing/app/scripts/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	$("#counter")
+		.countdown("2018/01/08 19:30:00", function(event) {
+			$(this).html(
+				event.strftime(
+					"<div><span class='value'>%-D</span><span class='line'></span><span class='unit'>días</div>" +
+						"<div><span class='value'>%-H</span><span class='line'></span><span class='unit'>horas</div>" +
+						"<div><span class='value'>%-M</span><span class='line'></span><span class='unit'>minutos</div>" +
+						"<div><span class='value'>%-S</span><span class='line'></span><span class='unit'>segundos</div>"
+				)
+			);
+		})
+		.on("update.countdown", function(event) {})
+		.on("finish.countdown", function(event) {});
+});
